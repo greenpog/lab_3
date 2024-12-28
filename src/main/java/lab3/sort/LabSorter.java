c68de8b50979a3d79e362e6877c5b96f8f0ab7ac
greenpog <182277176+greenpog@users.noreply.github.com>
2024-12-28 04:02:05 +0300

    Изменения в проекте

--- src/main/java/lab3/sort/LabSorter.java
+++ src/main/java/lab3/sort/LabSorter.java
@@ -9,7 +9,16 @@ public class LabSorter {
      * @return отсортированный массив
      */
     public int[] sort(int[] source) {
-
-        throw new RuntimeException("implement me");
+        int len = source.length;
+        for (int i=1; i<len; i++){
+            for(int j=1; j<=len-i; j++){
+                if (source[j-1]>source[j]){
+                    int swap =source[j];
+                    source[j]=source[j-1];
+                    source[j-1]=swap;
+                }
+            }
+        }
+        return source;
     }
 }
